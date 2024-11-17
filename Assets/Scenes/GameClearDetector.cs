39e4b0d7256a30aea2cda0461483821dbec5f453
rumia_as <rumia_as@yahoo.co.jp>
2024-11-17 18:24:27 +0900

    クローンして挙動を確認、OnGUIのパラメータを微調整

--- Assets/Scenes/GameClearDetector.cs
+++ Assets/Scenes/GameClearDetector.cs
@@ -9,9 +9,12 @@ public class GameClearDetector : MonoBehaviour
     public Hole holeGreen;
     //Holeクラスを付与されたコンポーネントを登録できるようになる
     void OnGUI(){
+        GUIStyle textStyle = new GUIStyle();
+        textStyle.fontSize = 20;
+        textStyle.normal.textColor = Color.white;
         GUI.matrix = Matrix4x4.Scale(Vector3.one*2);
         if (holeRed.IsHolding && holeBlue.IsHolding && holeGreen.IsHolding){
-            GUI.Label(new Rect(10, 10, 100, 30), "Game Clear!");
+            GUI.Label(new Rect(125, 45, 100, 30), "Game Clear!");
         }
     }
     // Start is called before the first frame update
